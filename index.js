58ea11408604f146541e6c6ceb8c4e771f02f01f
Vinn Nduri <ndurivo@gmail.com>
2022-06-19 08:32:21 +0300

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ if (word === word.split("").reverse().join("")){
 
 /* 
   Add your pseudocode here
+  write the function to check the word
+  slit the word, reverse, then join
+  Compare the word and the reversed version
+  if palindrome return true, if not return false
 */
-
 /*
   Add written explanation of your solution here
+  write the function that accepts the word as a parameter
+  in JS, .reverse only works in arrays and so the word is  slit, reversed then later joined
+  the resulting word is then compared against the one provided
+  If the two compare both forward and in reverse, its a palindrome hence return true, if not return false
+
 */
 
 // You can run `node index.js` to view these console logs
